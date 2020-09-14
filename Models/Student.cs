0983db5b1f6351e59a83b0f2cee86ea5f2ea0a10
saifafridi <saifafridi169@gmail.com>
2020-09-14 10:33:48 -0700

    first ommit

---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Net;
+
+namespace Saifudin.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string fathername { get; set; }
+        public string Gender { get; set; }
+        public string proggram { get; set; }
+        public decimal marks { get; set; }
+    }
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+
+
+}
